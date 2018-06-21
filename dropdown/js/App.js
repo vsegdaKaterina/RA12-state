1f18b6580e17f779be14e51aa8f4725c715f031e
Katerina <ekurochka@gmail.com>
2018-06-21 10:03:26 +0300

    Задача 3 Выпадающий список

---
+++ dropdown/js/App.js
@@ -0,0 +1,90 @@
+/*
+var App = React.createClass({
+  defaultProps: {
+    options: []
+  },
+  getInitialState: function () {
+    return {
+      active: this.props.options[0],
+      open: false
+    };
+  },
+  handleChange: function (option) {
+    this.setState({
+      active: option
+    });
+  },
+  toggleOpen: function () {
+    this.setState({
+      open: !this.state.open
+    });
+  },
+  render: function () {
+    return (
+      <div className="container">
+        <div className={`dropdown-wrapper ${this.state.open ? "open" : ""}`} >
+          <button className={"btn"} onClick={this.toggleOpen} >
+            <span>Account Settings</span>
+            <i className="material-icons">public</i>
+          </button>
+          <ul className="dropdown">
+            {this.props.options.map((option, i) => (
+              <li
+                className={option === this.state.active ? "active" : ""}
+                onClick={() => this.handleChange(option)} >
+                  <a href="#">{option}</a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    );
+  }
+});
+*/
+
+class App extends React.Component {
+  constructor (props) {
+    super(props);
+
+    if (!this.props.options) {
+      this.props.options = [];
+    }
+
+    this.state = {open: false, active: this.props.options[0]};
+  }
+
+  handleChange = option => {
+    this.setState({
+      active: option
+    });
+  };
+
+  toggleOpen = () => {
+    this.setState({
+      open: !this.state.open
+    });
+  };
+
+  render() {
+    return (
+      <div className="container">
+        <div className={`dropdown-wrapper ${this.state.open ? "open" : ""}`} >
+          <button className={"btn"} onClick={this.toggleOpen} >
+            <span>Account Settings</span>
+            <i className="material-icons">public</i>
+          </button>
+          <ul className="dropdown">
+            {this.props.options.map((option, i) => (
+              <li
+                className={option === this.state.active ? "active" : ""}
+                onClick={() => this.handleChange(option)} >
+                  <a href="#">{option}</a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    );
+  }  
+}
